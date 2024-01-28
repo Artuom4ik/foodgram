281005092bc84c38279197c1c462c7f9279ae981
Artuom4ik <masterskayaigr123@gmail.com>
2024-01-28 16:48:04 +0300

    Write Product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from categories.models import Card
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    scope_of_application = models.CharField(max_length=200, verbose_name='Сфера применения')
+    diametr = models.FloatField(verbose_name='Диаметр')
+    length = models.FloatField(verbose_name='Длинна')
+    image = models.ImageField(null=True, default='', verbose_name='Изображение')
+    card = models.ForeignKey(
+        Card,
+        on_delete=models.CASCADE,
+        related_name='products',
+        verbose_name='карточка'
+    )
+
+    def __str__(self) -> str:
+        return self.title
