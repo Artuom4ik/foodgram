3dc8d45f8eecb8c3d1191b36dbf79c4738265036
Artuom4ik <masterskayaigr123@gmail.com>
2024-01-28 16:34:21 +0300

    Write Group model

--- categories/models.py
+++ categories/models.py
@@ -2,7 +2,15 @@ from django.db import models
 
 
 class Section(models.Model):
-    title = models.CharField(max_length=200, verbose_name='')
-    description = models.TextField(blank=True, null=True, verbose_name='')
+    title = models.CharField(max_length=200, verbose_name='Название')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    section = models.ForeignKey(
+        Section,
+        on_delete=models.CASCADE,
+        related_name='groups',
+        verbose_name='Секция')
