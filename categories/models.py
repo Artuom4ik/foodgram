c4b637e6c13f265f0e365ebf04c51f2887961075
Artuom4ik <masterskayaigr123@gmail.com>
2024-01-28 16:43:20 +0300

    Write Subgroup model

--- categories/models.py
+++ categories/models.py
@@ -14,3 +14,14 @@ class Group(models.Model):
         on_delete=models.CASCADE,
         related_name='groups',
         verbose_name='Секция')
+    
+
+class Subgroup(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+        related_name='subgroups',
+        verbose_name='Группа')
+    
