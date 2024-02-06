77d3d7ba8ffc336fb826d75be42a579b3f615d2d
Artuom4ik <masterskayaigr123@gmail.com>
2024-02-06 11:46:02 +0300

    Fix detail information card api

--- categories/urls.py
+++ categories/urls.py
@@ -9,6 +9,6 @@ app_name = 'categories'
 
 urlpatterns = [
     path('', views.categories_list_api),
-    path('detail_information_card/<int:card_id>/', views.detail_information_card),
+    path('detail_information_card/<int:card_id>/', views.detail_information_card_api),
     path('products_list/<int:card_id>/', views.products_list_api),
 ]
--- categories/views.py
+++ categories/views.py
@@ -38,16 +38,16 @@ def detail_information_card_api(request, card_id):
             if product.scope_of_application not in detail_information['scope_of_application']:
                 detail_information['scope_of_application'].append(product.scope_of_application)
             
-            elif product.diametr not in detail_information['diameters']:
+            if product.diametr not in detail_information['diameters']:
                 detail_information['diameters'].append(product.diametr)
             
-            elif product.length not in detail_information['length']:
+            if product.length not in detail_information['length']:
                 detail_information['length'].append(product.length)
             
-            elif product.color not in detail_information['colors']:
+            if product.color not in detail_information['colors']:
                 detail_information['colors'].append(product.color)
             
-            elif product.image and not detail_information['image']:
+            if product.image and not detail_information['image']:
                 detail_information['image'] = product.image.url
         
         return JsonResponse(detail_information, safe=False, json_dumps_params={
