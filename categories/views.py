41ee971389ecb8fed7952576264e6ede3b49973b
Artuom4ik <masterskayaigr123@gmail.com>
2024-02-07 14:37:25 +0300

    Fix categories_list_api function

--- categories/views.py
+++ categories/views.py
@@ -8,7 +8,9 @@ def categories_list_api(request):
     sections = Section.objects.all()
 
     categories = {
-        'data': [{'section': {'title': section.title}} for section in sections]
+        'data': {
+            'sections': [{'section': {'title': section.title}} for section in sections],
+        }
     }
 
     return JsonResponse(categories, safe=False, json_dumps_params={
