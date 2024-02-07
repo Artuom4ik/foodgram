0a8b00fba0a82b106b8245f2dd6cc93d28e512b8
Artuom4ik <masterskayaigr123@gmail.com>
2024-02-07 13:58:30 +0300

    Add endpoint

--- categories/urls.py
+++ categories/urls.py
@@ -9,6 +9,7 @@ app_name = 'categories'
 
 urlpatterns = [
     path('', views.categories_list_api),
+    path('cards_list/section/<int:section_id>/group/<int:group_id>/subgroup/<int:subgroup_id>/', views.cards_list_api),
     path('detail_information_card/<int:card_id>/', views.detail_information_card_api),
     path('products_list/<int:card_id>/', views.products_list_api),
 ]
